13a3f82f218edd4084acb54d9c5e118b667603e8
marcmaroun <marcyves.maroun@gmail.com>
2017-03-24 11:02:58 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,103 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (cordova.platformId === "ios" && window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+          url: '/tab',
+          abstract: true,
+          templateUrl: 'templates/tabs.html'
+      })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.evenement', {
+        url: '/evenement',
+        views: {
+            'tab-evenement': {
+                templateUrl: 'templates/tab-evenement.html',
+                controller: 'EvenementCtrl'
+            }
+        }
+    })
+    
+    .state('tab.creerevenement', {
+        url: '/creerevenement',
+        views: {
+            'tab-creerevenement': {
+                templateUrl: 'templates/tab-creerevenement.html',
+                controller: 'creerevenementCtrl'
+            }
+        }
+    })
+
+    .state('tab.tab-evenementDetail', {
+            url: '/evenement/:evenementId',
+            views: {
+                'tab-evenement': {
+                    templateUrl: 'templates/tab-evenementDetail.html',
+                    controller: 'EvenementDetailCtrl'
+                }
+            }
+        })   
+    .state('tab.profil',{
+        url:'/profil/:userId',
+        views:{
+            'tab-profil':{
+                templateUrl:'templates/tab-profile.html',
+                controller:'profilCtrl'
+            }
+        }
+    })
+
+      ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/evenement');
+
+})
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#imgInp").change(function(){
+    readURL(this);
+});
---
+++ js/services.js
@@ -0,0 +1,61 @@
+﻿angular.module('starter.services', [])
+
+.factory('evenements', function ($window) {
+    if (typeof $window.localStorage.localEvenements == "undefined") {
+        var tmpEvenements = [
+            {
+                id_evenement: 0,
+                city_img: "/img/paris.jpg",
+                city_name: "Paris"
+                user_firstname: "Marc",
+                user_lastname: "maroun",
+                user_id:"1"
+                
+            },
+            {
+                id_evenement: 1,
+                city_img: "/img/ny.jpg",
+                city_name:"New-York",
+                city_name: "Paris"
+                user_firstname: "EL",
+                user_lastname: "Mehdi",
+                user_id:"2"
+                
+            },
+            {
+                id_evenement: 2,
+                city_img: "/img/londres.jpg",
+                city_name: "Londres",
+                city_name: "Paris"
+                user_firstname: "Marc",
+                user_lastname: "maroun",
+                user_id:"1"
+
+            }
+           
+        ];
+        $window.localStorage.localEvenements = JSON.stringify(tmpEvenements);
+    }
+    var evenements = JSON.parse($window.localStorage.localEvenements);
+    return {
+        all: function () {
+            return evenements;
+        },
+        get: function (evenementsId) {
+            for (var i = 0; i < evenements.length; i++) {
+                if (evenements[i].id_evenement === parseInt(evenementsId)) {
+                    return evenements[i];
+                }
+            }
+            return null;
+        },
+        add: function (evenement) {
+            evenements.push(evenement);
+        },
+        save: function () {
+            $window.localStorage.localEvenements = JSON.stringify(evenements);
+        },
+    };
+
+
+});
