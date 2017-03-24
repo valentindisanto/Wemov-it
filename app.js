31c9b8b71779fb770386d2609db1509ea632a3b7
marcmaroun <marcyves.maroun@gmail.com>
2017-03-24 10:56:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,25 +3,24 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (cordova.platformId === "ios" && window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
+
 
 
 
@@ -62,3 +61,83 @@ function previewFiles() {
   }
 
 }
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+          url: '/tab',
+          abstract: true,
+          templateUrl: 'templates/tabs.html'
+      })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.evenement', {
+        url: '/evenement',
+        views: {
+            'tab-evenement': {
+                templateUrl: 'templates/tab-evenement.html',
+                controller: 'EvenementCtrl'
+            }
+        }
+    })
+    
+    .state('tab.creerevenement', {
+        url: '/creerevenement',
+        views: {
+            'tab-creerevenement': {
+                templateUrl: 'templates/tab-creerevenement.html',
+                controller: 'creerevenementCtrl'
+            }
+        }
+    })
+
+    .state('tab.tab-evenementDetail', {
+            url: '/evenement/:evenementId',
+            views: {
+                'tab-evenement': {
+                    templateUrl: 'templates/tab-evenementDetail.html',
+                    controller: 'EvenementDetailCtrl'
+                }
+            }
+        })   
+    .state('tab.profil',{
+        url:'/profil/:userId',
+        views:{
+            'tab-profil':{
+                templateUrl:'templates/tab-profile.html',
+                controller:'profilCtrl'
+            }
+        }
+    })
+
+      ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/evenement');
+
+})
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#imgInp").change(function(){
+    readURL(this);
+});
